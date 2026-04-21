77bdf8fec139dae44467b17fc158d1899193c913
bhavanasatyasripulum-ux <bhavanasatyasripulum@gmail.com>
2026-04-21 14:37:28 +0530

    Add files via upload

--- running totals.py
+++
@@ -1,5 +0,0 @@
-numbers = [10,20,30,40]
-total = 0
-for num in numbers:
-    total = total + num
-    print("running total:",total)
--- square of number.py
+++
@@ -1,3 +0,0 @@
-n=int(input("Enter the limit:"))
-for i in range(1,1+n):
-    print(i,"square is",i*i)
